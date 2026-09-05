bf5730ad51747c7e6a23d9436827ba82762a9dd9
zengzengping-creator <zengzengping@gmail.com>
2026-09-05 22:17:59 +0800

    feat: increase enemy power-up drop rates

---
+++ drop-rates.js
@@ -0,0 +1,7 @@
+// 敌人道具掉落概率
+// 普通 60% / 快速 70% / 重甲 80% / 火力 90% / 精英 100%
+ENEMY_TYPES.normal.dropChance = 0.60;
+ENEMY_TYPES.fast.dropChance = 0.70;
+ENEMY_TYPES.armor.dropChance = 0.80;
+ENEMY_TYPES.firepower.dropChance = 0.90;
+ENEMY_TYPES.elite.dropChance = 1.00;
