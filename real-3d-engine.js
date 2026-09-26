b006e5f76715b429d138da4acd3bdbfb99cff31b
zengzengping-creator <zengzengping@gmail.com>
2026-09-26 21:30:19 +0800

    feat: move 3D amusement details closer to first island

--- real-3d-engine.js
+++ real-3d-engine.js
@@ -920,7 +920,7 @@
 
         // 6 彩虹跳台
         {
-          const pads=[[678,585],[695,568],[713,553],[732,540],[750,527],[768,513]];
+          const pads=[[648,565],[665,548],[683,533],[702,520],[720,507],[738,493]];
           const colors=[0xff6f86,0xffa84d,0xffe05b,0x6edb8a,0x61cfff,0x9a78ff];
           pads.forEach((q,i)=>{
             const p=islandCoord(q[0],q[1],0);
